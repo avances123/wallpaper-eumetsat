218866c2418661c0467e13fcf7fff48cbe1f3351
Fabio Rueda <avances123@gmail.com>
2011-10-10 21:12:30 +0200

    debianizing

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(wallpaper-eumetsat):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="wallpaper-eumetsat" command="/usr/bin/wallpaper-eumetsat"
---
+++ debian/package.default.ex
@@ -0,0 +1,10 @@
+# Defaults for wallpaper-eumetsat initscript
+# sourced by /etc/init.d/wallpaper-eumetsat
+# installed at /etc/default/wallpaper-eumetsat by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
