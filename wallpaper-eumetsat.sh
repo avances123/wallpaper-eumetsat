df25eb5f15017dd0114aa5c50b3f4a8a171dd0e6
Fabio Rueda <avances123@gmail.com>
2011-10-10 16:35:26 +0200

    Any size can be used as wallpaper

--- wallpaper-eumetsat.sh
+++ wallpaper-eumetsat.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#wget http://oiswww.eumetsat.org/IPPS/html/MSG/RGB/NATURALCOLOR/FULLDISC/IMAGESDisplay/2FramesNATURALCOLOR-FULLDISC.zip -O /tmp/escritorio.zip
-wget http://oiswww.eumetsat.org/IPPS/html/MSG/PRODUCTS/MPE/FULLDISC/IMAGESDisplay/2FramesMPE-FULLDISC.zip -O /tmp/escritorio.zip
-cd /tmp
-IMG=$(unzip -o escritorio.zip | grep extracti| head -1 | cut -f3 -d' ')
-echo IMAGEN $IMG
-convert $IMG -crop 800x775+0+0 /home/fa/escritorio.jpg
+source /etc/wallpaper-eumetsat.conf
+wget $URL -O $TEMPFILE
+geom=$(identify -verbose $TEMPFILE | grep Geom | cut -d: -f'2' | cut -f1 -d+ | sed -e 's/ //g')
+width=$(echo $geom | cut -f1 -dx)
+heigth=$(echo $geom | cut -f2 -dx)
+newheigth=$(expr $heigth - 25)
+echo convert $TEMPFILE -crop ${width}x${newheigth}+0+0 $WALLPAPERIMG
+convert $TEMPFILE -crop ${width}x${newheigth}+0+0 $WALLPAPERIMG
 gconftool-2 --type string --set /desktop/gnome/background/picture_options scaled
-gconftool-2 --type string --set /desktop/gnome/background/picture_filename /home/fa/escritorio.jpg
-rm -f /tmp/escritorio.zip
+gconftool-2 --type string --set /desktop/gnome/background/picture_filename $WALLPAPERIMG
+rm -f $TEMPFILE
